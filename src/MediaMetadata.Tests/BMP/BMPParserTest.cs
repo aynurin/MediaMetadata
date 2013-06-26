ee244b96d370abcdb0a115a060dc22b8544e5091
Shavkat Aynurin <shavkat@ainurin.com>
2013-06-27 01:55:32 +0400

    Initial commit

---
+++ src/MediaMetadata.Tests/BMP/BMPParserTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+
+namespace MediaMetadata.Tests
+{
+    [TestClass]
+    public class BMPParserTest
+    {
+        [TestMethod]
+        public void ProcessTestFiles()
+        {
+            var asm = Assembly.GetExecutingAssembly();
+            var m1 = MediaParser.ExtractMetadata(asm.GetManifestResourceStream("MediaMetadata.Tests.BMP.Data.a2c38d9af9fcdac5377bda307da5abb2.bmp"));
+            Assert.AreEqual(m1.Size.Width, 685);
+            Assert.AreEqual(m1.Size.Height, 610);
+        }
+    }
+}
